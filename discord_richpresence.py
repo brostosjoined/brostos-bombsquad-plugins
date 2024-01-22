6c72a9b00bd9ff80b6498cf91eac7f2823a873ec
brostos <67740566+brostosjoined@users.noreply.github.com>
2024-01-22 20:33:39 +0300

    Move token so the plugin works after update or reinstall without login in again

--- discord_richpresence.py
+++ discord_richpresence.py
@@ -374,12 +374,13 @@ def get_event_loop(force_fresh=False):
 
         def _generate_join_secret(self):
             # resp = requests.get('https://legacy.ballistica.net/bsAccessCheck').text
-            connection_info = bs.get_connection_to_host_info() if build_number < 21697 else bs.get_connection_to_host_info_2()
-            if connection_info:
-                addr = _last_server_addr
-                port = _last_server_port
-            else:
-                try:
+            try:
+                connection_info = bs.get_connection_to_host_info(
+                ) if build_number <  21727 else bs.get_connection_to_host_info_2()
+                if connection_info:
+                    addr = _last_server_addr
+                    port = _last_server_port
+                else:
                     with urlopen(
                         "https://legacy.ballistica.net/bsAccessCheck"
                     ) as resp:
@@ -393,8 +394,8 @@ def get_event_loop(force_fresh=False):
                         "port": port,
                     }
                     self.join_secret = json.dumps(secret_dict)
-                except:
-                    pass
+            except:
+                pass
 
         def _update_secret(self):
             threading.Thread(target=self._generate_join_secret, daemon=True).start()
@@ -875,7 +876,8 @@ class DiscordRP(babase.Plugin):
 
     def update_status(self) -> None:
         roster = bs.get_game_roster()
-        connection_info = bs.get_connection_to_host_info() if build_number < 21697 else bs.get_connection_to_host_info_2()
+        connection_info = bs.get_connection_to_host_info(
+        ) if build_number <  21727 else bs.get_connection_to_host_info_2()
 
         self.rpc_thread.large_image_key = "bombsquadicon"
         self.rpc_thread.large_image_text = "BombSquad"
